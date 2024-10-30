1d5e711342fe74a48f12720320682adbaa2835db
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-10-29 22:48:38 -0300

    harpoon marks custom

--- config.lua
+++ config.lua
@@ -186,8 +186,16 @@ lvim.plugins = {
   {
     'ThePrimeagen/harpoon',
     config = function()
-      vim.keymap.set('n', '<leader>h',
-        '<cmd>lua require("harpoon.mark").add_file()<CR><cmd>lua print("File added to list ⇁")<CR>')
+      local function goto_harpoon(index)
+        return '<cmd>lua require("harpoon.mark").set_current_at(' ..
+            index .. ')<CR><cmd>lua print("File added to list ⇁ ' .. index .. '")<CR>'
+      end
+
+      vim.keymap.set('n', '<leader>hh', goto_harpoon(1))
+      vim.keymap.set('n', '<leader>hj', goto_harpoon(2))
+      vim.keymap.set('n', '<leader>hk', goto_harpoon(3))
+      vim.keymap.set('n', '<leader>hl', goto_harpoon(4))
+
       vim.keymap.set('n', '<leader><leader>', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>')
       vim.keymap.set('n', '1', '<cmd>lua require("harpoon.ui").nav_file(1)<CR>')
       vim.keymap.set('n', '2', '<cmd>lua require("harpoon.ui").nav_file(2)<CR>')
